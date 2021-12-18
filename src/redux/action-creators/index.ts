8652ff3810c37859bda332bc2bf13e105dca76f2
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-18 23:08:56 +0500

    Create action creators and fix dispatch issue

---
+++ src/redux/action-creators/index.ts
@@ -0,0 +1,29 @@
+import { ActionTypes } from '../action-types';
+import { Dispatch } from 'redux';
+import { Action, IObject, RemoveBookPayload } from '../actions';
+import axios from 'axios';
+
+export const addBook = (payload: IObject) => (dispatch: Dispatch<Action>) => {
+  axios
+    .post(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/lFhMyFEx06tkxSEStL98/books',
+      payload
+    )
+    .then(() => dispatch({ type: ActionTypes.ADD_BOOK, payload }));
+};
+
+export const removeBook = (payload: RemoveBookPayload) => {
+  return (dispatch: Dispatch<Action>) => {
+    dispatch({ type: ActionTypes.REMOVE_BOOK, payload });
+  };
+};
+
+export const loadBooks = () => (dispatch: Dispatch<Action>) => {
+  axios
+    .get(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/lFhMyFEx06tkxSEStL98/books'
+    )
+    .then((response) =>
+      dispatch({ type: ActionTypes.LOAD_BOOKS, payload: response.data })
+    );
+};
